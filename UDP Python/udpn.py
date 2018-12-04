68a89e9c03c0d776fd214c3155a2098ef05616ef
Daniel <daniel_katz@hotmail.ca>
2018-12-04 02:49:30 -0800

    Workng version

--- UDP Python/udpn.py
+++ UDP Python/udpn.py
@@ -12,7 +12,6 @@ ports = 8001
 bufsize = 1024
 packetList = []
 ackList = []
-
 sockobjc = socket(AF_INET, SOCK_DGRAM)
 sockobjs = socket(AF_INET, SOCK_DGRAM)
 sockobjc.bind(("", portc))
@@ -23,33 +22,34 @@ print("Waiting on traffic from: ", portc, "& ", ports)
 
 if len(sys.argv) == 3:
     host = sys.argv[1]
-    print 'Random drop chance: ' + sys.argv[2]
+    print 'Random drop chance: ' + sys.argv[2] + '%'
 elif len(sys.argv) == 2:
-    print 'Random drop chance: ' + sys.argv[1]
+    print 'Random drop chance: ' + sys.argv[1] + '%'
+    dropChance = sys.argv[1]
     print 'Using Default of localhost'
 
 datac, addrc = sockobjc.recvfrom(bufsize)
 
 while (datac):
     packet = Packet.decode(Packet(), datac)
-    print 'Received Packet Type: ' + str(packet.getPacketType()) + ", Sequence Number: " + str(packet.getSeqNum())
+    #print 'Received Packet Type: ' + str(packet.getPacketType()) + ", Sequence Number: " + str(packet.getSeqNum())
     packetList.append(packet)
     if (packet.getPacketType() == "EOT"):
         for pckt in packetList:
             sockobjs.sendto(pckt.toString(), (host, ports))
-            print 'Sent Packet Type: ' + str(pckt.getPacketType()) + ", Sequence Number: " + str(pckt.getSeqNum())
+            #print 'Sent Packet Type: ' + str(pckt.getPacketType()) + ", Sequence Number: " + str(pckt.getSeqNum())
         packetList = []
         datas, addrs = sockobjs.recvfrom(bufsize)
         while (datas):
             ack = Packet.decode(Packet(), datas)
-            print 'Received Packet Type: ' + str(ack.getPacketType()) + ", Acknowledgement Number: " + str(ack.getAckNum())
+            #print 'Received Packet Type: ' + str(ack.getPacketType()) + ", Acknowledgement Number: " + str(ack.getAckNum())
             ackList.append(ack)
             datas, addrs = sockobjs.recvfrom(bufsize)
             if (datas == "EOT"):
                 break
         for acks in ackList:
             sockobjc.sendto(acks.toString(), addrc)
-            print 'Sent Packet Type: ' + str(acks.getPacketType()) + ", Acknowledgement Number: " + str(acks.getAckNum())
+            #print 'Sent Packet Type: ' + str(acks.getPacketType()) + ", Acknowledgement Number: " + str(acks.getAckNum())
         ackList = []
         sockobjc.sendto("EOT", addrc)
     datac, addrc = sockobjc.recvfrom(bufsize)
--- UDP Python/udps.py
+++ UDP Python/udps.py
@@ -2,21 +2,19 @@
 
 from socket import *
 from packet import Packet
-import sys
 import pickle
 
 host = "localhost"
 port = 8001
 bufsize = 1024
 packetList = []
-
 sockobj = socket(AF_INET, SOCK_DGRAM)
 sockobj.bind(("", port))
 
 #print("Server")
 
 print("waiting on port:", port)
-f = open("hello1.txt", 'wb')
+f = open("test1.png", 'wb')
 
 if len(sys.argv) == 2:
     host = sys.argv[1]
@@ -32,7 +30,7 @@ while (data):
     f.write(packet.getData())
     if (packet.getPacketType() == "EOT"):
         for pckt in packetList:
-            ack = Packet("ACK", 1, pckt.getData(), pckt.getSeqNum())
+            ack = Packet("ACK", 1, "", pckt.getSeqNum())
             ackStr = ack.toString()
             sockobj.sendto(ackStr, addr)
             print 'Sent Packet Type: ' + str(ack.getPacketType()) + ", Acknowledgement Number: " + str(ack.getAckNum())
